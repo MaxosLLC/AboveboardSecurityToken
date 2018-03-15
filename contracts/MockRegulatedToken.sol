5092bca9611a3e7a3c2fc721a15f44772663c3ab
vhromek <vojtar1@gmail.com>
2018-03-15 15:17:12 +0100

    Contract mocks

---
+++ contracts/MockRegulatedToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.18;
+
+import 'contracts/RegulatedToken.sol';
+import 'contracts/RegulatorService.sol';
+
+contract MockRegulatedToken is RegulatedToken {
+  RegulatorService public service;
+  uint public decimals;
+
+  // 0xffffffff is a test address for ServiceRegistry that is bypassed by our _service() implementation
+  function MockRegulatedToken(address _service) public
+    RegulatedToken(ServiceRegistry(0xffffffff), "MockToken", "MTKN")
+  {
+    service = RegulatorService(_service);
+  }
+
+  function setDecimals(uint _decimals) public {
+    decimals = _decimals;
+  }
+
+  function _service() constant public returns (RegulatorService) {
+    return service;
+  }
+}
---
+++ contracts/MockRegulatorService.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import 'contracts/RegulatorService.sol';
+
+contract MockRegulatorService is RegulatorService  {
+  bool public success;
+  uint8 public reason;
+
+  function setCheckResult(bool _success, uint8 _reason) public {
+    success = _success;
+    reason = _reason;
+  }
+
+  function check(address, address, address, address, uint256) public returns (uint8) {
+    return reason;
+  }
+}
