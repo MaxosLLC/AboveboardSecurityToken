fea252fcaaa546bd75ee380260e51589066ecdce
vhromek <vojtar1@gmail.com>
2018-03-26 16:06:33 +0200

    Solidity Coverage

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,13 @@ module.exports = {
       port: process.env.TRUFFLE_PORT || 8545,
       network_id: '*' // Match any network id
     },
-
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
     rinkeby: getRinkebyConfig()
 
   }
