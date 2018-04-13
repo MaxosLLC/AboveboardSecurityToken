f43e341aa0a1b04a0ea74163fcf336d1c220dc5f
vhromek <vojtar1@gmail.com>
2018-04-13 14:23:38 +0200

    MultiSigArbitration Migration

--- migrations/4_multiSigArbitration.js
+++ migrations/4_multiSigArbitration.js
@@ -1,5 +1,11 @@
+var RegulatedToken = artifacts.require("./RegulatedToken.sol");
 var MultiSigArbitration = artifacts.require('contracts/MultiSigArbitration.sol');
 
 module.exports = async function(deployer, network, accounts) {
-    return deployer.deploy(MultiSigArbitration, [accounts[0]], 1);
+    return deployer.deploy(MultiSigArbitration, [accounts[0]], 1)
+      .then(() => {
+        return RegulatedToken.deployed().then(function(instance) {
+          return instance.setMultisigArbitrator(MultiSigArbitration.address);
+        });
+      });
 };
