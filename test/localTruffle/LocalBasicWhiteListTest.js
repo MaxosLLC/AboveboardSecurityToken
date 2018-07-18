8c38c32bc099057ab81784d194b6ec25af12719f
vhromek <vojtar1@gmail.com>
2018-07-18 08:49:32 +0200

    IssuanceWhiteList onlyAgentOrOwnerOrQualifier

--- test/localTruffle/LocalBasicWhiteListTest.js
+++ test/localTruffle/LocalBasicWhiteListTest.js
@@ -1,26 +1,8 @@
-let BasicWhiteList = artifacts.require('contracts/BasicWhiteList.sol');
+const BasicWhiteList = artifacts.require('./BasicWhiteList.sol');
 
 contract('BasicWhiteList', function(accounts) {
 
     let basicWhiteList;
-
-    //Sometimes throws error when importing from helpers
-
-    const assertEvent = function(contract, filter) {
-        return new Promise((resolve, reject) => {
-            var event = contract[filter.event]();
-            event.watch();
-            event.get((error, logs) => {
-                var log = _.filter(logs, filter);
-                if (log) {
-                    resolve(log);
-                } else {
-                    throw Error("Failed to find filtered event for " + filter.event);
-                }
-            });
-            event.stopWatching();
-        });
-    }
 
     beforeEach(async () => {
 
@@ -30,16 +12,13 @@ contract('BasicWhiteList', function(accounts) {
 
     it("Test basic functions in BasicWhiteList", async function() {
         
-        await basicWhiteList.add(accounts[0]);
-
-        await assertEvent(basicWhiteList, 
-            { event: "MemberAdded", logIndex: 0, args: { member: accounts[0] }});
+        let w = await basicWhiteList.add(accounts[0]);
+        assert.equal(w.logs[0].event, 'MemberAdded');
 
         assert.equal(await basicWhiteList.verify(accounts[0]), true);
-        await basicWhiteList.remove(accounts[0]);
 
-        await assertEvent(basicWhiteList, 
-            { event: "MemberRemoved", logIndex: 1, args: { member: accounts[0] }});
+        w = await basicWhiteList.remove(accounts[0]);
+        assert.equal(w.logs[0].event, 'MemberRemoved');
 
         assert.equal(await basicWhiteList.verify(accounts[0]), false);
 
