6313e1a3a1d0b9dfe1add9c2f0e5cd949f906f04
vhromek <vojtar1@gmail.com>
2018-07-25 13:35:53 +0200

    RegulatedToken transferFrom and arbitrage

--- contracts/RegulatedToken.sol
+++ contracts/RegulatedToken.sol
@@ -19,6 +19,11 @@ contract RegulatedToken is DetailedERC20, MintableToken {
    */
   event CheckStatus(uint8 reason, address indexed spender, address indexed from, address indexed to, uint256 value);
 
+ /**
+   * @notice Triggered when transfered by arbitrage
+  */
+  event Arbitrage(address _from, address _to, uint256 _value);
+
   /**
    * @notice Address of the `ServiceRegistry` that has the location of the
    *         `RegulatorService` contract responsible for checking trade
@@ -55,8 +60,8 @@ contract RegulatedToken is DetailedERC20, MintableToken {
     }
   }
 
-  /**
-   * @notice Transfer tokens from one address to another
+ /**
+   * @notice ERC-20 overridden function that include logic to check for trade validity.
    *
    * @param _from The address of the sender
    * @param _to The address of the receiver
@@ -64,20 +69,34 @@ contract RegulatedToken is DetailedERC20, MintableToken {
    *
    * @return `true` if successful and `false` if unsuccessful
    */
-  function transferFrom(address _from, address _to, uint256 _value) onlyOwner public returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    if (_checkTransferFrom(_from, _to, _value)) {
+      return super.transferFrom(_from, _to, _value);
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * @notice Transfer tokens by arbitrage from one address to another
+   *
+   * @param _from The address of the sender
+   * @param _to The address of the receiver
+   * @param _value The number of tokens to transfer
+   *
+   * @return `true` if successful and `false` if unsuccessful
+   */
+  function arbitrage(address _from, address _to, uint256 _value) onlyOwner public returns (bool) {
     require(_to != address(0));
     require(_from != address(0));
     require(_value <= balances[_from]);
 
-    if (_checkTransferFrom(_from, _to, _value)) {
-      balances[_from] = balances[_from].sub(_value);
-      balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
 
-      Transfer(_from, _to, _value);
-      return true;
-    } else {
-      return false;
-    }
+    Arbitrage(_from, _to, _value);
+    Transfer(_from, _to, _value);
+    return true;
   }
 
   /**
