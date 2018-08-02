1d63bcf8f6c698c554fc8b0d506f361038e4e929
Jordan Denison <jordan@denisonweb.com>
2018-08-02 10:48:51 -0500

    add affiliates whitelist to deploy script, lock truffle and ethereumjs wallet versions

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 const IssuanceWhiteList = artifacts.require('./IssuanceWhiteList.sol')
+const SecureIssuanceWhiteList = artifacts.require('./SecureIssuanceWhiteList.sol')
 const SettingsStorage = artifacts.require('./SettingsStorage.sol')
 const RegulatorService = artifacts.require('./AboveboardRegDSWhitelistRegulatorService.sol')
 const RegulatedToken = artifacts.require('./RegulatedToken.sol')
@@ -6,13 +7,17 @@ const ServiceRegistry = artifacts.require('./ServiceRegistry.sol')
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(IssuanceWhiteList)
+  await deployer.deploy(SecureIssuanceWhiteList)
   await deployer.deploy(SettingsStorage)
   await deployer.deploy(RegulatorService, SettingsStorage.address)
   await deployer.deploy(ServiceRegistry, RegulatorService.address)
   await deployer.deploy(RegulatedToken, ServiceRegistry.address, 'AboveboardStock', 'ABST')
 
   const whitelist = await IssuanceWhiteList.deployed()
-  await whitelist.setWhitelistType('RegS')
+  await whitelist.setWhitelistType('Affiliates')
+
+  const seucreWhitelist = await SecureIssuanceWhiteList.deployed()
+  await seucreWhitelist.setWhitelistType('RegS')
 
   const storage = await SettingsStorage.deployed()
   await storage.addWhitelist(IssuanceWhiteList.address)
